3281298e495a5642bf8fcde4d11ccdeadea558a7
Mauro Wildemberg <129429885+Mauroufes@users.noreply.github.com>
2023-04-27 20:07:46 -0300

    Create Fera.java

---
+++ Fera.java
@@ -0,0 +1,24 @@
+public class Fera extends Combatente {
+
+	public Fera(String identificacao) {
+		super(identificacao);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int morder() {
+		return 20;
+	}
+	
+	public int usarGarra() {
+		return 10;
+	}
+	public void atacar(Combatente adversario) {		
+		if (Math.random() > 0.5) {
+			adversario.defender(this.morder());
+		}else {
+			adversario.defender(this.usarGarra());
+		}
+	}
+	
+
+}
