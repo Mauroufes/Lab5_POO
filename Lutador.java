6d783500d57ce24df6b91bc1ca665f7d59ffbd08
Mauro Wildemberg <129429885+Mauroufes@users.noreply.github.com>
2023-04-27 20:08:59 -0300

    Create Lutador.java

---
+++ Lutador.java
@@ -0,0 +1,24 @@
+public class Lutador extends Combatente {
+
+	public Lutador(String identificacao) {
+		super(identificacao);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int chutar() {
+		return 20;
+	}
+	
+	public int socar() {
+		return 10;
+	}
+	public void atacar(Combatente adversario) {		
+		if (Math.random() > 0.5) {
+			adversario.defender(this.chutar());
+		}else {
+			adversario.defender(this.socar());
+		}
+	}
+	
+
+}
